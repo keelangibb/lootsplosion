be9354ace07ea9466702f79df5583776cabfe1f7
keelangibb <keelangibb@gmail.com>
2023-04-03 00:13:19 -0500

    db changes, working weapons getall endpoint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import { type NextPage } from "next";
+import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
+  const { data: weapons } = api.weapons.getAll.useQuery();
+  console.log(weapons);
   return (
     <div>
       <h1>Home</h1>
--- src/server/api/routers/weapons.ts
+++ src/server/api/routers/weapons.ts
@@ -3,7 +3,20 @@ import { getByIdSchema } from "~/shared/schema/weapons.schema";
 
 export const weaponsRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.weapon.findMany();
+    return ctx.prisma.weapon.findMany({
+      include: {
+        elementCombination: {
+          select: {
+            elements: {
+              select: { name: true, picture: true },
+            },
+          },
+        },
+        manufacturer: { select: { name: true } },
+        rarity: { select: { name: true } },
+        type: { select: { name: true } },
+      },
+    });
   }),
   getById: publicProcedure.input(getByIdSchema).query(({ ctx, input }) => {
     return ctx.prisma.weapon.findUnique({
