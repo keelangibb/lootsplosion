505b4d48d065a37b0e3f1bd6b05563ce2c613a8f
keelangibb <keelangibb@gmail.com>
2023-04-02 22:18:11 -0500

    prisma models

--- src/server/api/routers/weapons.ts
+++ src/server/api/routers/weapons.ts
@@ -1,7 +1,13 @@
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { getByIdSchema } from "~/shared/schema/weapons.schema";
 
-export const exampleRouter = createTRPCRouter({
+export const weaponsRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
+    return ctx.prisma.weapon.findMany();
+  }),
+  getById: publicProcedure.input(getByIdSchema).query(({ ctx, input }) => {
+    return ctx.prisma.weapon.findUnique({
+      where: { id: input.weaponId },
+    });
   }),
 });
