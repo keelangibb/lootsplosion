adbbf7c19ad985e8e30418d76b7c0f93437dfb04
keelangibb <keelangibb@gmail.com>
2023-04-04 21:14:49 -0500

    Basic Navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,41 +1,84 @@
+import Link from "next/link";
+
 export default function NavBar() {
   return (
     <div className="navbar bg-base-200">
-      <div className="flex-1">
-        <a className="btn-ghost btn text-xl normal-case">Lootsplosion</a>
-      </div>
-      <div className="flex-none gap-2">
-        <div className="form-control">
-          <input
-            type="text"
-            placeholder="Search"
-            className="input-bordered input"
-          />
-        </div>
-        <div className="dropdown-end dropdown">
-          <label tabIndex={0} className="btn-ghost btn-circle avatar btn">
-            <div className="w-10 rounded-full">
-              <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-            </div>
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn-ghost btn-circle btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h7"
+              />
+            </svg>
           </label>
           <ul
             tabIndex={0}
             className="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
           >
             <li>
-              <a className="justify-between">
-                Profile
-                <span className="badge">New</span>
-              </a>
+              <a>Homepage</a>
             </li>
             <li>
-              <a>Settings</a>
+              <a>Portfolio</a>
             </li>
             <li>
-              <a>Logout</a>
+              <a>About</a>
             </li>
           </ul>
         </div>
+      </div>
+      <div className="navbar-center">
+        <Link href="/" className="btn-ghost btn text-xl normal-case">
+          <span className="text-orange-400">Loot</span>
+          <span className="">splosion</span>
+        </Link>
+      </div>
+      <div className="navbar-end">
+        <button className="btn-ghost btn-circle btn">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </button>
+        <button className="btn-ghost btn-circle btn">
+          <div className="indicator">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+              />
+            </svg>
+            <span className="badge-primary badge badge-xs indicator-item"></span>
+          </div>
+        </button>
       </div>
     </div>
   );
